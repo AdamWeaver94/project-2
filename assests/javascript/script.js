53f3a031a3caa641c7afd89daddfe84d8727df9f
Adam Weaver <adamweaver@live.co.uk>
2023-05-31 18:44:08 +0000

    add js first function to game

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,3 +1,10 @@
 // first step is to make cards turn when clicking, followed by card active period will be until next card is clicked.
 // If correct, will hold cards over and remain visible, if wrong cards will flip back around and return to starting poisiton.
 
+const cards = document.querySelectorAll('.card');
+
+function flipcard() {
+    this.classlist.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipcard));
