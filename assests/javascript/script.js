facb07c8c595ec49d52d088d372a4a4a8d4a6e67
Adam Weaver <adamweaver@live.co.uk>
2023-05-31 17:34:24 +0000

    add style to cards

---
+++ assests/javascript/script.js
@@ -0,0 +1,3 @@
+// first step is to make cards turn when clicking, followed by card active period will be until next card is clicked.
+// If correct, will hold cards over and remain visible, if wrong cards will flip back around and return to starting poisiton.
+
